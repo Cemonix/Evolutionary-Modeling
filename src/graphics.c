7255a887a122dbf9ada14de59ff053eec59c5bb4
Milan Gittler <gittlermilan@seznam.cz>
2024-02-24 15:08:20 +0100

    Animation of ants moving

--- src/graphics.c
+++ src/graphics.c
@@ -7,15 +7,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <utils.h>
 
 #include "raymath.h"
-
-#define MAX_CITIES 5
 
 City cities[MAX_CITIES];
 int citiesCount = 0;
 
-void initButton(Button* button, const char* label, const int x, const int y, const int width, const int height)
+void InitButton(Button* button, const char* label, const int x, const int y, const int width, const int height)
 {
     button->bounds = (Rectangle){ x, y, width, height };
     strcpy(button->label, label);
@@ -23,7 +22,7 @@ void initButton(Button* button, const char* label, const int x, const int y, con
     button->isPressed = false;
 }
 
-void drawCityWithLabel(const Vector2 position, const int cityIndex, const float circleRadius)
+void DrawCityWithLabel(const Vector2 position, const int cityIndex, const float circleRadius)
 {
     char label[20];
     sprintf(label, "City: %d", cityIndex);
@@ -32,7 +31,7 @@ void drawCityWithLabel(const Vector2 position, const int cityIndex, const float 
     DrawText(label, position.x + 12, position.y - 5, 10, BLACK);
 }
 
-void drawAntMovement(Ant* ant, const City* cities, const float radius)
+void DrawAntMovement(Ant* ant, const City* cities, const float radius)
 {
     const Vector2 startPos = cities[ant->previousCity].position;
     const Vector2 endPos = cities[ant->currentCity].position;
@@ -43,7 +42,7 @@ void drawAntMovement(Ant* ant, const City* cities, const float radius)
 
     DrawCircleV(interpolatedPos, radius, RED);
 
-    ant->progress += 0.02;
+    ant->progress += ANIMATION_SPEED;
     // Movement completed
     if (ant->progress >= 1.0) {
         ant->progress = 0.0;
@@ -51,13 +50,8 @@ void drawAntMovement(Ant* ant, const City* cities, const float radius)
     }
 }
 
-void createCityMatrix(double** cityMatrix, const City* cities, const int citiesCount)
+void FillCityMatrix(double** cityMatrix, const City* cities, const int citiesCount)
 {
-    for (int i = 0; i < citiesCount; i++)
-    {
-        cityMatrix[i] = (double*) malloc(citiesCount * sizeof(double));
-    }
-
     for (int i = 0; i < citiesCount; i++) {
         for (int j = 0; j < citiesCount; j++) {
             if (i == j)
@@ -68,21 +62,25 @@ void createCityMatrix(double** cityMatrix, const City* cities, const int citiesC
     }
 }
 
-void initGraphicsWindow()
+void InitGraphicsWindow()
 {
-    bool matrixInited = false;
+    bool matrixInitilized = false;
     bool animating = false;
+
     Button startSimulationButton;
-    initButton(
+    InitButton(
         &startSimulationButton, "Start Simulation",
-        SCREEN_WIDTH - 210, SCREEN_HEIGHT * 0.05, 200, 40
+        SCREEN_WIDTH * 0.725, SCREEN_HEIGHT * 0.05, 200, 40
     );
+    const Rectangle leftPanel = {SCREEN_WIDTH * 0.7, 0,SCREEN_WIDTH * 0.3,SCREEN_HEIGHT};
+    const Color leftPanelColor = {225, 225, 225, 250};
 
     Ant ants[NUM_ANTS];
     double** cityMatrix = nullptr;
-    double pheromoneMatrix[NUM_CITIES][NUM_CITIES];
-
-    initializeSimulation(ants, pheromoneMatrix);
+    double** pheromoneMatrix = nullptr;
+    size_t bestTour = INT_MAX;
+    char* tourLabel = SafeMalloc(100 * sizeof(char));
+    strcpy(tourLabel, "Best tour lenght:");
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "ACO Visualization");
     SetTargetFPS(60);
@@ -92,10 +90,15 @@ void initGraphicsWindow()
         const Vector2 mousePosition = GetMousePosition();
         if (CheckCollisionPointRec(mousePosition, startSimulationButton.bounds))
             startSimulationButton.isHovered = true;
-        else if (!startSimulationButton.isPressed)
-        {
+        else
             startSimulationButton.isHovered = false;
 
+        if (startSimulationButton.isHovered && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            startSimulationButton.isPressed = true;
+        }
+
+        if (!CheckCollisionPointRec(mousePosition, leftPanel))
+        {
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && citiesCount < MAX_CITIES)
             {
                 cities[citiesCount].position = mousePosition;
@@ -103,34 +106,80 @@ void initGraphicsWindow()
             }
         }
 
-        if (startSimulationButton.isHovered && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            startSimulationButton.isPressed = true;
-        }
-
-        if (startSimulationButton.isPressed)
+        if (startSimulationButton.isPressed && citiesCount > 1)
         {
-            if (!matrixInited)
+            if (!matrixInitilized)
             {
-                cityMatrix = (double**) malloc(citiesCount * sizeof(double*));
-                createCityMatrix(cityMatrix, cities, citiesCount);
-                matrixInited = true;
+                cityMatrix = (double**) SafeMalloc(citiesCount * sizeof(double*));
+                pheromoneMatrix = (double**) SafeMalloc(citiesCount * sizeof(double*));
+                if (pheromoneMatrix == nullptr)
+                {
+                    printf("Memory allocation for pheromone matrix failed");
+                    exit(1);
+                }
+
+                for (int i = 0; i < citiesCount; i++)
+                {
+                    cityMatrix[i] = (double*) SafeMalloc(citiesCount * sizeof(double));
+                    pheromoneMatrix[i] = (double*) SafeMalloc(citiesCount * sizeof(double));
+                }
+
+                FillCityMatrix(cityMatrix, cities, citiesCount);
+                InitializeSimulation(ants, pheromoneMatrix, citiesCount);
+
+                matrixInitilized = true;
             }
 
-            if (!animating && !allVisited(ants[0].visited, citiesCount))
+            if (!animating)
             {
-                antMove(&ants[0], cityMatrix, citiesCount, pheromoneMatrix);
-                animating = true;
+                int numAntPathFinished = 0;
+                for (int i = 0; i < NUM_ANTS; ++i)
+                {
+                    if (!AllVisited(ants[i].visited, citiesCount))
+                        AntMove(&ants[i], cityMatrix, citiesCount, pheromoneMatrix);
+                    else
+                        numAntPathFinished++;
+                }
+
+                if (numAntPathFinished == NUM_ANTS)
+                {
+                    for (int i = 0; i < NUM_ANTS; ++i)
+                    {
+                        if (ants[i].tourLength < bestTour)
+                            bestTour = ants[i].tourLength;
+                    }
+
+                    DepositPheromones(ants, pheromoneMatrix, citiesCount);
+                    EvaporatePheromones(pheromoneMatrix, citiesCount);
+                    InitializeAnts(ants, citiesCount);
+                }
+                else
+                    animating = true;
             }
             else
             {
-                drawAntMovement(&ants[0], cities, 5);
-                if (ants[0].previousCity == ants[0].currentCity)
-                    animating = false;
+                for (int i = 0; i < NUM_ANTS; ++i)
+                {
+                    DrawAntMovement(&ants[i], cities, 5);
+                }
+
+                for (int i = 0; i < NUM_ANTS; ++i)
+                {
+                    animating = (ants[i].previousCity != ants[i].currentCity);
+                    if (animating)
+                        break;
+                }
             }
         }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        DrawRectangleRec(leftPanel, leftPanelColor);
+
+        if (bestTour < INT_MAX)
+            sprintf(tourLabel, "Best tour lenght: %llu", bestTour);
+        DrawText(tourLabel, 10, 10, 20, BLACK);
 
         if (startSimulationButton.isHovered)
             DrawRectangleRec(startSimulationButton.bounds, GRAY);
@@ -152,12 +201,15 @@ void initGraphicsWindow()
 
         for (int i = 0; i < citiesCount; ++i)
         {
-            drawCityWithLabel(cities[i].position, i, 10);
+            DrawCityWithLabel(cities[i].position, i, 10);
         }
 
         EndDrawing();
     }
 
     CloseWindow();
+
+    FreeAnts(ants);
     free(cityMatrix);
+    free(pheromoneMatrix);
 }
